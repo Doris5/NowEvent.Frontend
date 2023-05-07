9678e8ee03952929d30e6e970aa2c00d43e02e48
Doris5 <rmajchrzak7@o2.pl>
2023-05-07 20:08:03 +0200

    Add Benefit Section

--- react/src/app/layout/App.tsx
+++ react/src/app/layout/App.tsx
@@ -1,14 +1,16 @@
 import { useState } from 'react';
 import './style.css';
-import { Container, createTheme, CssBaseline } from '@mui/material';
+import { createTheme, CssBaseline, PaletteMode } from '@mui/material';
 import { ThemeProvider } from '@emotion/react';
 import { ToastContainer } from 'react-toastify';
 import Navbar from '../components/navbar/Navbar';
 import { Outlet } from 'react-router-dom';
 
 export default function App() {
-    const [darkMode, setDarkMode] = useState(false);
-    const palleteType = darkMode ? 'dark' : 'light';
+    const [darkMode, setDarkMode] = useState<boolean>(false);
+
+    const palleteType: PaletteMode = darkMode ? 'dark' : 'light';
+
     const theme = createTheme({
         palette: {
             mode: palleteType,
